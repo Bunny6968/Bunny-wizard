4c6051d4595abad917c8566c990ec32f281f6435
BUNNY <106491296+Bunny6968@users.noreply.github.com>
2022-09-18 06:18:41 +0000

    Idk

--- run.sh
+++ run.sh
@@ -8,6 +8,20 @@ echo ╚═════╝░░╚═════╝░╚═╝░░╚══
 echo Creator : Būnny Discord : Būnny@1233
 sleep 2
 PS3="Enter Your Selection :"
-select i in Nuker Gen 
-
+select i in Gen Exit
+do
+if [ $i == 'Exit' ]
+then 
+echo Shutting Down Wizard..
+sleep 1
+exit
+fi
+if [ $i == 'Gen' ]
+then 
+echo Starting Gen....
+sleep 1
+echo Soon!
+exit
+fi
+done 
 
